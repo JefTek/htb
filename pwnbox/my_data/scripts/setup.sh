aec1ec28d7b6552a61d071f11f091d06213024bd
Jef Kazimer <jef@jeftek.com>
2021-02-07 18:57:14 -0600

    Create setup.sh

---
+++ pwnbox/my_data/scripts/setup.sh
@@ -0,0 +1,9 @@
+sudo apt update -y
+sudo apt install masscan
+sudo apt upgrade nmap -y
+sudo apt install snapd
+sudo snap install core
+sudo snap install cherrytree
+sudo snap install zaproxy --classic
+sudo apt upgrade -y
+sudo apt autoremove -y
